9be7c7194ec3e414b51af3be3a92fa8e29127466
Phuc Nguyen <contact@phucnguyen.info>
2017-01-27 00:42:35 +0700

    Remove unnecessary files

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'npvn:jquery-bar-rating',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'jquery-bar-rating packaged for Meteor',
   // URL to the Git repository containing the source code for this package.
